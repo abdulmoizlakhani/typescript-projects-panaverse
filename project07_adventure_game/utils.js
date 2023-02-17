a9dca8b894378b3d00b38670e651ef240074c77a
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2023-02-17 22:06:56 +0500

    - deleted map files

--- project07_adventure_game/utils.js
+++
@@ -1,4 +0,0 @@
-export function getRandomNumber(max) {
-    return Math.floor(Math.random() * max) + 1;
-}
-//# sourceMappingURL=utils.js.map
--- project07_adventure_game/utils.mjs
+++
@@ -1,4 +0,0 @@
-export function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
-}
-//# sourceMappingURL=utils.mjs.map
